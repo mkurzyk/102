a0fb361c5957a4f9f39c4815d926fdfc65455165
Mariusz Kurzyk <mariusz.kurzyk@gmail.com>
2018-04-20 18:17:53 +0200

    new files added

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function () {
+    console.log('DOM loaded - you can have fun')
+    var span = $('span')
+    $('span:even').css('color', 'red');
+    var paragraphs = $('p');
+    paragraphs.each(function (index, element) {
+        var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+        $(element).append(button);
+    });
+    $('button').click(function () {
+        alert($(this).attr('data-tmp'));
+    });
+
+})
